f05d12348afeb3c2ed8b5586d1f77bfaea4a18e4
Sameer-Marathe <marathe.k.sameer@gmail.com>
2020-06-18 15:43:09 -0400

    upload docker done

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=sam2g/dev
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag yourtag99 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
